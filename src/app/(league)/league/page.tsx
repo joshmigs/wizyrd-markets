7023831961255b296656feba4156e8e5bf315fdb
Josh Migliardi <joshuamigliardi@gmail.com>
2026-01-28 01:22:27 -0500

    Force dynamic rendering for league/playground

--- src/app/(league)/league/page.tsx
+++ src/app/(league)/league/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState, type FormEvent, type MouseEvent } from "react";
